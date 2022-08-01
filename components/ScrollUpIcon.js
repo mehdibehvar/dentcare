4c749222c56ed4b7afa67c969ab8acf296d7cb00
mehdibehvar <mehdibehvar00@gmail.com>
2022-08-01 19:54:30 +0430

    add-scrollup-Icon

---
+++ components/ScrollUpIcon.js
@@ -0,0 +1,34 @@
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import {  Box, IconButton, Tooltip } from "@mui/material";
+import  { useEffect, useState } from "react";
+export default function ScrollUpIcon() {
+    const [showTooltip,setShowTooltip]=useState("hidden");
+    const handleScrollUp=()=>{
+      window.scroll({
+        top: 0,
+        right: 0,
+        behavior: 'smooth'
+      });
+    };
+    const handleShowScrollUpIcon=()=>{
+      if(window.scrollY<=150){
+        setShowTooltip("hidden");
+      }else{
+        setShowTooltip("visible")
+      }
+    }
+    useEffect(()=>{
+      document.addEventListener("scroll",handleShowScrollUpIcon);
+  
+      return ()=> document.removeEventListener("scroll",handleShowScrollUpIcon)
+    },[])
+  return (
+    <Box sx={{position:"fixed",bottom:"0",visibility:showTooltip}}>
+    <Tooltip title="برو بالای صفحه">
+       <IconButton onClick={handleScrollUp} color="secondary" sx={{backgroundColor:"primary.main"}}>
+         <KeyboardArrowUpIcon />
+       </IconButton>
+     </Tooltip>
+</Box>
+  )
+}
--- components/layout.js
+++ components/layout.js
@@ -1,21 +1,21 @@
-import Head from 'next/head'
-import React from 'react'
-import { Container } from '@mui/material'
-import { Box } from '@mui/system'
-import TopBar from './topbar/TopBar'
+import Head from "next/head";
+import { Box } from "@mui/system";
+import TopBar from "./topbar/TopBar";
+import ScrollUpIcon from "./ScrollUpIcon";
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
   return (
     <>
-        <Head>
-            <title>BitaCare website</title>
-            <meta name="viewport" content="initial-scale=1, width=device-width" />
-            <meta name='mehdishop'  content='best dentist in bushehr' />
-        </Head>
-        <TopBar/>
-          <Box sx={{backgroundColor:"#FFFFF"}}>
-          {children}
-          </Box>
+      <Head>
+        <title>BitaCare website</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta name="mehdishop" content="best dentist in bushehr" />
+      </Head>
+      <TopBar />
+      <Box sx={{ backgroundColor: "#FFFFF" }}>
+        {children}
+ <ScrollUpIcon/>
+      </Box>
     </>
-  )
+  );
 }
