e4015fd6600ba9977e2e74f3d9a8f34c123e6976
mehdibehvar <mehdibehvar00@gmail.com>
2023-07-05 12:39:29 +0330

    noref

--- components/banner/Banner.js
+++ components/banner/Banner.js
@@ -1,57 +1,58 @@
-import styled from "@emotion/styled"
-import { Box, Button } from "@mui/material"
-import Navbar from "components/navbar/Navbar"
-import BannerDetails from "./BannerDetails"
-import ReactSvgLogo from "./ReactLogo"
+import styled from "@emotion/styled";
+import { Box, Button } from "@mui/material";
+import Navbar from "components/navbar/Navbar";
+import BannerDetails from "./BannerDetails";
+import ReactSvgLogo from "./ReactLogo";
 export default function Banner() {
-    const BannerSection=styled("section")(({theme})=>({
-        backgroundImage:"url(./assets/images/bann.jpg)",
-        position: "relative",
-        backgroundSize: "cover",
-        overflow: "hidden",
-        backgroundAttachment: "inherit",
-        height: "86vh",
-        backgroundPosition: "62% 0%",
-        [theme.breakpoints.up("md")]:{
-            height:"93vh",
-            backgroundPosition:"70% 0"
-        },
-        [theme.breakpoints.down("md")]:{
-            height:"86vh",
-            backgroundPosition:"70% 0"
-        },
-        [theme.breakpoints.down('sm')]:{
-            height:"80vh",
-            backgroundPosition:"70% 0"
-        },
-        [theme.breakpoints.down("xs")]:{
-            height:"72vh",
-            backgroundPosition:"70% 0"
-        }
-    }));
-    const MyLogo=styled("div")(({theme})=>({
-        width:70,
-        height:50,
-        backgroundColor:"primary.main",
-        position:"absolute",
-        top:170,
-        left:300,
-    }));
-    const handleOpensys=()=>{
-        window.open('http://www.google.com', '_system');
-    }
+  const BannerSection = styled("section")(({ theme }) => ({
+    backgroundImage: "url(./assets/images/bann.jpg)",
+    position: "relative",
+    backgroundSize: "cover",
+    overflow: "hidden",
+    backgroundAttachment: "inherit",
+    height: "86vh",
+    backgroundPosition: "62% 0%",
+    [theme.breakpoints.up("md")]: {
+      height: "93vh",
+      backgroundPosition: "70% 0",
+    },
+    [theme.breakpoints.down("md")]: {
+      height: "86vh",
+      backgroundPosition: "70% 0",
+    },
+    [theme.breakpoints.down("sm")]: {
+      height: "80vh",
+      backgroundPosition: "70% 0",
+    },
+    [theme.breakpoints.down("xs")]: {
+      height: "72vh",
+      backgroundPosition: "70% 0",
+    },
+  }));
+  const MyLogo = styled("div")(({ theme }) => ({
+    width: 70,
+    height: 50,
+    backgroundColor: "primary.main",
+    position: "absolute",
+    top: 170,
+    left: 300,
+  }));
+  const handleOpensys = () => {
+    window.open("http://www.google.com", "_system");
+  };
   return (
-   <BannerSection>
-   <Box>
-   <a href="https://master.tube/apply/#apply-forms" target="_system">  درخواست نقد کردن درآمد کانال</a>
-   <Button onClick={handleOpensys}>
-    go bro
-   </Button>
-    <MyLogo>
-        <ReactSvgLogo/>
-    </MyLogo>
-   <BannerDetails/>
-   </Box>
-   </BannerSection>
-  )
+    <BannerSection>
+      <Box>
+        <a href="https://master.tube/apply/#apply-forms" target="_blank" rel="noreferrer">
+          {" "}
+          درخواست نقد کردن درآمد کانال
+        </a>
+        <Button onClick={handleOpensys}>go bro</Button>
+        <MyLogo>
+          <ReactSvgLogo />
+        </MyLogo>
+        <BannerDetails />
+      </Box>
+    </BannerSection>
+  );
 }
